823c88bb79828c3baf4651fa17934a12f8a7f7f8
Brian Sadler <briansadler@MacBook-Pro-3.local>
2018-11-02 09:47:30 -0500

    removed game details fully

---
+++ VGL-IOS/View Controllers/GameDetailsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameDetailsViewController.swift
+//  VGL-IOS
+//
+//  Created by Brian Sadler on 11/2/18.
+//  Copyright © 2018 Brian Sadler. All rights reserved.
+//
+
+import UIKit
+
+class GameDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionTextLog: UITextView!
+    
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    @IBOutlet weak var checkedInOrOut: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+           var selectedGame: VideoGame
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
